17764ca6ba70afb344b3bf3d2bf2fc439fe13ff2
PennyGuyDyls <dylanprothero@icloud.com>
2026-03-25 17:23:59 +0000

    some words added

--- Wordle/Assets/Nothing.py
+++ Wordle/Assets/Nothing.py
@@ -5,16 +5,27 @@ while True:
     word=f.readline().strip().upper()
     if word=='':
         break
-    if word not in words and len(word)==5:
-        words.append(word)
-    else: print(word)
+    words.append(word)
+
 
 
 f.close()
 
+f=open('Wordle/Assets/answers.txt','r')
+
+while True:
+    word=f.readline().strip().upper()
+    if word=='':
+        break
+    if word not in words:
+        words.append(word)
+
+f.close()
+
+words.sort()
 f=open('Wordle/Assets/guesses.txt','w')
+
 for i in words:
     f.write(i+'\n')
 
-f.close()
-
+f.close
